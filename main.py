8df0e6f197a04e4278397758c4b5a650c513c5fa
Victor Apolinares <FINANZAS01@forum.local>
2023-08-28 16:02:43 -0400

    Me quede en el video 13.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, flash
 
 # Creamos la instancia de Flask
 app = Flask(__name__)
 
+@app.before_request
+def before_request():
+    print("Antes de la petición")
+@app.after_request
+def after_request(response):
+    print("Despues de la petición")
+    return response
 @app.route('/')
 def index():
-    encabezado = "Encabezado desde flask"
+    flash('Has iniciado en el index del proyecto Flask')
+    print("Accediendo al index o pagina principal")
     datos = {'titulo': 'Pagina principal',
             'encabezado': 'Bienvienido a mi pagina web'}
     return render_template('index.html', dato = datos)
@@ -22,7 +30,17 @@ def redirecciona(sitio = None):
 @app.route('/acercade')
 def acercade():
     #return "<h1>Acerca de mi</h1>"
-    return render_template('acercade.html')
+    diccionario = {'title': 'Acerca de',
+                'encabezado': 'Acerca de mi'}
+    return render_template('acercade.html', datos = diccionario)
+
+@app.route('/condicionybucle')
+def condicionybucle():
+    datos = {
+        'edad': 50,
+        'nombre': ['Jose','Mar', 'Lucia', 'Eva']
+    }
+    return render_template('condicionybucle.html', datos = datos)
 
 @app.route('/saludame')
 @app.route('/saludame/<string:nombre>')
@@ -44,6 +62,10 @@ def saludame(nombre = 'Victor', edad = None):
 def suma(num1, num2):
     return f"La suma es: {num1 + num2}"
 
+def pagina_no_encontrada(error):
+    return render_template('errors/404.html'), 404
 
 if __name__ == '__main__':
+    app.register_error_handler(404, pagina_no_encontrada)
+    app.secret_key = 'clave-flask'
     app.run(debug= True, port=8080)
